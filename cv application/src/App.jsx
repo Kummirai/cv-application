70a30e8f02b4c62da146d2e9758a678f08bf31db
Kummirai <ajaxmilton@hotmail.com>
2024-10-20 15:19:26 +0200

    Refactor App component to include NameSection component and update CSS styles

--- cv application/src/App.jsx
+++ cv application/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import NameSection from "./components/CvTemplate/NameSection";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <NameSection />
     </div>
   );
 }
---
+++ cv application/src/components/CvTemplate/NameSection.jsx
@@ -0,0 +1,8 @@
+import "./nameSection.css"
+const NameSection = () => {
+  return (
+    <div className="cvTemplate">NameSection</div>
+  )
+}
+
+export default NameSection
